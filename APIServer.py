94fa6458886e94c5a6cefada0bc09930ae034d49
kiton_Chen <chenpidaxia@gmail.com>
2018-02-10 16:37:50 +0800

    添加darknet的yolo模型，增加一个route

--- APIServer.py
+++ APIServer.py
@@ -1,4 +1,28 @@
-from flask import Flask
+#coding:utf-8
+#导入包
+import sys,os
+sys.path.append(os.path.join(os.getcwd(),'darknet/python'))
+#导入darknet和
+import darknet as dn_handle
+from flask import Flask,make_response,jsonify
+
+#设置GPU
+#dn_handle.set_gpu(0)
+#加载网络和元数据
+net,meta = dn_handle.init()
+
 app = Flask(__name__)
 
-@app.route()
+@app.route('/dectect',methods=['POST'])
+def dectect():
+    try:
+       # date  = request.
+    except Exception as e:
+        return repr(e),500
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Not Found'}),404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WebAPISysofML
 使用Flask开发的基于ML的WebAPI系统
+算法端使用:yolo框架
